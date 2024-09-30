744939f95972b910da8c75b306afe64f07dcfa44
duycop <duycop@tnut.edu.vn>
2024-09-30 15:40:14 +0700

    Update huong_duong.cpp

--- code/huong_duong.cpp
+++ code/huong_duong.cpp
@@ -1,50 +1,45 @@
 #include<iostream>	
 using namespace std;
 int main(){
-	cout<<"pm tinh so ngay huong tho\n"
-	    <<"nhap d1,m1,y1: ";
-	int d1,m1,y1,d2,m2,y2;
-	cin>>d1>>m1>>y1;
-	cout<<"nhap d2,m2,y2: ";
-	cin>>d2>>m2>>y2;
-	
-	int dem  = 0;  //bien chua so ngay dem dc
-	
-	while(!(d1==d2 && m1==m2 && y1==y2)){ //dk lap: ngay1 not equal ngay2
-		dem++;  //tang 1 ngay dem duoc
-		d1=d1+1;  //tang gia tri bien NGAY
-		
-		int max_ngay_trong_thang;
-		switch(m1){
+	cout<<"PHAN MEM TINH SO NGAY HUONG DUONG\n";	//thông tin về chương trình này, bài toán này.
+	cout<<"Nhap DATE1 (d1,m1,y1): ";		//thông báo trước khi nhập date1
+	int d1,m1,y1,d2,m2,y2;				//khai báo trước khi nhập
+	cin>>d1>>m1>>y1;				//đợi người dùng nhập 3 số nguyên vào 3 biến
+	cout<<"Nhap Date2 (d2,m2,y2): ";		//khai báo trước khi nhập
+	cin>>d2>>m2>>y2;				//đợi người dùng nhập 3 số nguyên vào 3 biến	
+	int dem = 0;  					//Khởi tạo biến đếm  = 0 chứa số ngày sẽ đếm	
+	while(!(d1==d2 && m1==m2 && y1==y2)){		//điều kiện lặp: Nếu date1 KHÔNG BẰNG date2	
+		int max_ngay_trong_thang;		//biến này sẽ tính số ngày MAX của tháng m1
+		switch(m1){				//áp dụng thuật toán tính số ngày của 1 tháng
 			case 1:
 			case 3:
 			case 5:
 			case 7:
 			case 8:
 			case 10:
-			case 12: max_ngay_trong_thang=31;
+			case 12: max_ngay_trong_thang=31;	//những tháng này có 31 ngày
 					 break;
 			case 4:
 			case 6:
 			case 9:
-			case 11: max_ngay_trong_thang=30;
+			case 11: max_ngay_trong_thang=30;	//những tháng này có 30 ngày
 					 break;
-			case 2:  if((y1%4==0 && y1%100!=0)||(y1%400==0))
-					 	max_ngay_trong_thang=29;
-					 else
-					 	max_ngay_trong_thang=28;
+			case 2:  if((y1%4==0 && y1%100!=0)||(y1%400==0))	//năm y1 nhuận
+					 	max_ngay_trong_thang=29;	//tháng 2: 29 ngày
+					 else					//năm ko nhuận
+					 	max_ngay_trong_thang=28;	//tháng 2: 28 ngày
 					 break;
 		} //end switch
-		
-		if(d1>max_ngay_trong_thang){ //neu qua ngay cuoi cung trong thang m1
-			d1=1;  //thi do la ngay 1 cua thang tiep theo
-			m1++;	//thang tiep theo la m1++
-			if(m1==13){  //tiep theo cua thang 12 la thang 1, ko phai thang 13
-				m1=1;  //tuc la thang 1 
-				y1++;  //cua nam tiep theo
-			}
-		} 
-	
+		dem++;  				//đếm thêm được 1 ngày
+		d1=d1+1;  				//tăng sang ngày mai (tomorow)
+		if(d1>max_ngay_trong_thang){ 		//nếu ngày mai lại vượt quá ngày MAX của tháng
+			d1=1;  				//thì đó là ngày 1 của tháng tiếp theo
+			m1++;				//tháng tiếp theo là m1++
+			if(m1>12){  			//m1 đang là 12 thì m++ sẽ thành 13
+				m1=1;  			//thì cần chỉnh lại là tháng 1
+				y1++;  			//của năm tiếp theo
+			}//end if m1 over 12
+		}//end if d1 over max
 	}//end while
-	cout<<"HUONG DUONG: "<<dem<<" ngay";	
+	cout<<"HUONG DUONG: "<<dem<<" ngay";		//thông báo kết quả ra màn hình sô ngày đếm được
 }
